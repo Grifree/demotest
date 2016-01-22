fc30b0748099a6d06e2f0f21ea492b36edceb4bc
Grifree <1149290648@qq.com>
2016-01-22 09:22:36 +0800

    add 离服务器可看html

---
+++ language_offline/language_en.js
@@ -0,0 +1,13 @@
+var data_en ={
+	"sign_comma" : "," ,
+	"sign_colon" : ":" ,
+	"sign_excalm" : "!" ,
+	"msg_language" : "language" ,
+	"msg_backreport" : "Feedback Report" ,
+	"msg_unsubcribed" : "Unsubcribed" ,
+	"msg_cause" : "Cause" ,
+	"msg_refused" : "Refused" ,
+	"msg_help_sent" : "If you need help, please contact us!" ,
+	"msg_explain_sent" : "Here is data form back-end" ,
+	"msg_success" : "success" 
+}
